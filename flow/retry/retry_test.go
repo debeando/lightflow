066b301873a4256cb11947f0677b1c72ecdda6e8
Nicola Strappazzon C <ns@thehotelsnetwork.com>
2020-10-04 21:48:26 +0200

    add test for retry

---
+++ flow/retry/retry_test.go
@@ -0,0 +1,39 @@
+package retry_test
+
+import (
+	"testing"
+
+	"github.com/swapbyt3s/lightflow/flow/retry"
+)
+
+func TestRetryOK(t *testing.T) {
+	var counter = 0
+
+	retry.Retry(
+		3,
+		1,
+		func () bool {
+			counter++
+			return true
+		})
+
+	if counter != 3 {
+		t.Errorf("Expected %d, got %d.", 3, counter)
+	}
+}
+
+func TestRetryKO(t *testing.T) {
+	var counter = 0
+
+	retry.Retry(
+		3,
+		1,
+		func () bool {
+			counter++
+			return false
+		})
+
+	if counter != 1 {
+		t.Errorf("Expected %d, got %d.", 1, counter)
+	}
+}
