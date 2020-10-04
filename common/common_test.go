d9c6bcd07b676d9ce53db05744902b99a019a6c3
Nicola Strappazzon C <ns@thehotelsnetwork.com>
2020-10-04 22:02:56 +0200

    add test for common.TestTrimNewlines

--- common/common_test.go
+++ common/common_test.go
@@ -7,14 +7,20 @@ import (
 )
 
 func TestTrimNewlines(t *testing.T) {
-	var cases = []string{
-		"abc123",
-		"\nabc123\n",
-		"\n\nabc123\n\n",
-		"\n\nabc\n123\n\n",
+	type TestTexts struct {
+		Text string
+		Result string
 	}
 
-	for _, c := range cases {
-		t.Log(common.TrimNewlines(c))
+	var testTexts = map[int]TestTexts{}
+	testTexts[0] = TestTexts{Text: "abc123",           Result: "abc123"}
+	testTexts[1] = TestTexts{Text: "\nabc123\n",       Result: "abc123"}
+	testTexts[2] = TestTexts{Text: "\n\nabc123\n\n",   Result: "abc123"}
+	testTexts[3] = TestTexts{Text: "\n\nabc\n123\n\n", Result: "abc\n123"}
+
+	for index, _ := range testTexts {
+		if common.TrimNewlines(testTexts[index].Text) != testTexts[index].Result {
+			t.Errorf("Expected %s, got %s.", testTexts[index].Result, common.TrimNewlines(testTexts[index].Text))
+		}
 	}
 }
