2e009f65403bc0cd8061abd1d4bf5ea3f480c3e7
Nicola Strappazzon C <ns@thehotelsnetwork.com>
2020-10-11 22:30:16 +0200

    #4 add list tasks, subtask and pipes.

--- cli/cli.go
+++ cli/cli.go
@@ -30,6 +30,7 @@ Options:
   --dry-run    No execute commands.
   --example    Print out full sample configuration to stdout.
   --help       Show this help.
+  --list       List tasks, subtask and pipes.
   --pipes      Filter by pipe name.
   --subtask    Filter by subtask name.
   --task       Filter by task name.
@@ -56,15 +57,21 @@ func Run() {
 	fDryRun := flag.Bool("dry-run", false, "")
 	fExample := flag.Bool("example", false, "")
 	fHelp := flag.Bool("help", false, "")
+	fList := flag.Bool("list", false, "")
 	fVariables := flag.String("variables", "", "")
 	fVersion := flag.Bool("version", false, "")
 	_ = flag.Bool("debug", false, "")
-	_ = flag.String("subtask", "", "")
 	_ = flag.String("pipes", "", "")
+	_ = flag.String("subtask", "", "")
 	_ = flag.String("task", "", "")
 
 	flag.Usage = func() { help(1) }
 	flag.Parse()
+
+	if err := config.Load().Read(*fConfig); err != nil {
+		log.Error(err.Error(), nil)
+		os.Exit(1)
+	}
 
 	switch {
 	case *fVersion:
@@ -76,13 +83,20 @@ func Run() {
 		fmt.Printf(example.GetConfigFile())
 	case len(*fAIDate) > 0 && len(*fVariables) > 0:
 		help(0)
+	case *fList:
+		for task_index := range config.Load().Tasks {
+			fmt.Println("* Task:", config.Load().Tasks[task_index].Name)
+			for subtask_index := range config.Load().Tasks[task_index].Subtask {
+				fmt.Println("  > Subtask:", config.Load().Tasks[task_index].Subtask[subtask_index].Name)
+				for pipe_index := range config.Load().Tasks[task_index].Pipes {
+					fmt.Println("    - Pipe:", config.Load().Tasks[task_index].Pipes[pipe_index].Name)
+				}
+			}
+		}
+
+		os.Exit(0)
 	case *fDryRun == true:
 		log.Warning("Running in safe mode, not execute any commands, only print commands.", nil)
-	}
-
-	if err := config.Load().Read(*fConfig); err != nil {
-		log.Error(err.Error(), nil)
-		os.Exit(1)
 	}
 
 	flow := flow.Flow{}
